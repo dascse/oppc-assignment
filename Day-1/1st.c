2ea93df177e1f7ceae8a674540b617ec001ca057
Jyotirmoy DAS <145799966+dascse@users.noreply.github.com>
2024-09-25 20:03:35 +0530

    Update 1st.c

--- Day-1/1st.c
+++ Day-1/1st.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <string.h>
 
+int linearsearch(char stops[][20], int numstops, const char* target) {
+    int i;
+    for (i = 0; i < numstops; i++) {
+        if (strcmp(stops[i], target) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    int numstops, i;
+    printf("Enter the number of stops: ");
+    scanf("%d", &numstops);
+
+    char stops[numstops][20];
+    printf("Enter the names of the stops:\n");
+    for (i = 0; i < numstops; i++) {
+        scanf("%s", stops[i]);
+    }
+}
